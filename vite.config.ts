3d9177dc74dd4338c71670503f085dd40d223955
waynemamahit <wayne.mamahit@gmail.com>
2024-08-29 22:05:48 +0700

    [Complete] Setup million.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@
 
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
+import million from 'million/compiler';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [
+    react(),
+    legacy(),
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ['useBadHook', /badVariable/g],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
